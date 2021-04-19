8df01014de5e8e5ac7f8d8803a4a973b67411330
Ved <dev@vedat.xyz>
2021-04-19 22:21:00 +0300

    Core Events again

---
+++ src/main/java/xyz/vedat/castleraid/event/CastleRaidCoreEvents.java
@@ -0,0 +1,138 @@
+package xyz.vedat.castleraid.event;
+
+import org.bukkit.Material;
+import org.bukkit.Sound;
+import org.bukkit.entity.Monster;
+import org.bukkit.entity.Player;
+import org.bukkit.event.EventHandler;
+import org.bukkit.event.Listener;
+import org.bukkit.event.block.Action;
+import org.bukkit.event.block.BlockBreakEvent;
+import org.bukkit.event.entity.CreatureSpawnEvent;
+import org.bukkit.event.entity.EntityRegainHealthEvent;
+import org.bukkit.event.entity.CreatureSpawnEvent.SpawnReason;
+import org.bukkit.event.entity.EntityRegainHealthEvent.RegainReason;
+import org.bukkit.event.inventory.InventoryClickEvent;
+import org.bukkit.event.inventory.InventoryType;
+import org.bukkit.event.player.PlayerDropItemEvent;
+import org.bukkit.event.player.PlayerInteractEvent;
+import org.bukkit.event.player.PlayerMoveEvent;
+
+import xyz.vedat.castleraid.CastleRaidMain;
+import xyz.vedat.castleraid.CastleRaidPlayer;
+import xyz.vedat.castleraid.classes.ClassItemFactory;
+
+public class CastleRaidCoreEvents implements Listener {
+    
+    CastleRaidMain plugin;
+    
+    public CastleRaidCoreEvents(CastleRaidMain plugin) {
+        
+        this.plugin = plugin;
+        
+    }
+    
+    
+    @EventHandler
+    public void onPlayerMove(PlayerMoveEvent event) {
+        
+        Player player = event.getPlayer();
+        CastleRaidPlayer crPlayer = plugin.getCrPlayers().get(player.getUniqueId());
+        
+        if (crPlayer == null) {
+            return;
+        }
+        
+        if (crPlayer.isCarryingBeacon() && event.getTo().getBlock().getLocation().equals(plugin.getBeaconTarget())) {
+            plugin.getLogger().info("Won the game");
+            plugin.startNewWorld();
+        }
+        
+    }
+    
+    @EventHandler
+    public void onBlockInteracted(PlayerInteractEvent event) {
+        
+        if (event.getAction().equals(Action.LEFT_CLICK_AIR) || event.getAction().equals(Action.RIGHT_CLICK_AIR)) {
+            return;
+        }
+        
+        if (event.getClickedBlock().getLocation().equals(plugin.getBeaconLocation())) {
+            plugin.getLogger().info("Beacon was interacted");
+            onBeaconGrabbed(event);
+        }
+        
+    }
+    
+    @EventHandler
+    public void onItemThrown(PlayerDropItemEvent event) {
+        
+        event.setCancelled(true);
+        
+    }
+    
+    @EventHandler
+    public void onInventoryClick(InventoryClickEvent event) {
+        
+        if (event.getCurrentItem() == null) {
+            return;
+        }
+        
+        // Prevent armor slots and class picker modifications
+        if (event.getSlotType().equals(InventoryType.SlotType.ARMOR) || event.getCurrentItem().isSimilar(ClassItemFactory.getClassPickerItem())) {
+            event.setCancelled(true);
+        }
+        
+        // Prevent putting stuff into chests, etc.
+        if (event.getInventory().getType() != InventoryType.PLAYER) {
+            event.setCancelled(true);
+        }
+        
+    }
+    
+    @EventHandler
+    public void onBlockBroken(BlockBreakEvent event) {
+        
+        plugin.getBuilderClaymores().remove(event.getBlock().getLocation());
+        
+    }
+    
+    @EventHandler
+    public void onHealthGained(EntityRegainHealthEvent event) {
+        
+        if (event.getRegainReason() == RegainReason.SATIATED && event.getEntity() instanceof Player) event.setCancelled(true);
+        
+    }
+    
+    @EventHandler
+    public void onMobSpawn(CreatureSpawnEvent event) {
+        
+        if (event.getSpawnReason() == SpawnReason.NATURAL && event.getEntity() instanceof Monster) event.setCancelled(true);
+        
+    }
+    
+    /**
+     * Not an actual event, it gets called by the event listener though.
+     * @param event The interaction event
+     */
+    public void onBeaconGrabbed(PlayerInteractEvent event) {
+        
+        Player player = event.getPlayer();
+        CastleRaidPlayer crPlayer = plugin.getCrPlayers().get(player.getUniqueId());
+        
+        if (crPlayer == null) {
+            return;
+        }
+        
+        if (event.getAction().equals(Action.LEFT_CLICK_BLOCK) && crPlayer.getTeam() == CastleRaidMain.Teams.RED && event.getClickedBlock().getType() == Material.BEACON) {
+            plugin.getLogger().info("Dude grabbed beacon");
+            player.getWorld().playSound(player.getLocation(), Sound.IRONGOLEM_DEATH, 10000, 0.5f);
+            event.getClickedBlock().setType(Material.AIR);
+            crPlayer.setCarriesBeacon(true);
+        } else {
+            event.setCancelled(true);
+        }
+        
+    }
+    
+}
